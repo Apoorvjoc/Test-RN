917de1d6809659ad4ad08d9fc313682a8d5d26f3
Apoorv <hlwapoorv@gmail.com>
2021-12-10 11:54:30 +0530

    making api call in RN

--- App.js
+++ App.js
@@ -1,12 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState , useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
+import axios from 'axios';
 
 export default function App() {
+  
+  const [isLoading, setLoading] = useState(true);
+  const [Data, setData] = useState([
+    // {id:1 , name:"mb" , email:"m@m.com"},
+    // {id:2 , name:"aj" , email:"a@a.com"},
+    // {id:3 , name:"pn" , email:"p@p.com"},
+    // {id:4 , name:"aj" , email:"a@a.com"},
+    // {id:5 , name:"mb" , email:"m@m.com"},
+    // {id:6 , name:"pn" , email:"p@p.com"},
+  ])
+  const [err, setErr] = useState('')
+
+  // useEffect(()=>{
+  //   fetch('https://jsonplaceholder.typicode.com/users')
+  //   .then((response)=>response.json())
+  //   .then((json)=>setData(json))
+  //   .catch((err)=> console.error("error in fetching : ", err))
+  //   .finally(()=> setLoading(false))
+  // },[]);
+
+  useEffect(()=>{
+      apiCall();
+  } , [])
+
+  const apiCall = async()=>{
+    try{
+      const response = await axios('https://jsonplaceholder.typicode.com/users');
+      console.log(response.data[0]);
+      setData(response.data)
+      setLoading(false);
+    }catch(err){
+      setLoading(false);
+      setErr(err.message);
+    }
+
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {
+       isLoading ? err!=''? <Text>{err}</Text> : <Text>Loading...</Text>
+       :
+          // <FlatList
+          // data = {Data}
+          // keyExtractor={item => item.id}
+          // renderItem={({item})=><Text>{item.email}</Text>}
+          // />
+          Data.map((item)=>(
+             <Text key={item.id}>
+            {item.name}{item.email}
+             </Text>
+            // <Text key={item.id}>{item.email}</Text>
+          ))
+     }
     </View>
   );
 }
